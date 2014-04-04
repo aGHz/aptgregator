aGHz/exosapient_old

Initialized repository from WebCore template

bin/initd.sh
@@ -1,0 +1,61 @@
+#! /bin/sh
+
+DIR=/Users/tek/src/exosapient
+USER=__user__
+GROUP=__group__
+
+PIDFILE=$DIR/var/run/production.pid
+LOGFILE=$DIR/var/log/console.log
+PROJECT=`basename $DIR`
+
+do_start()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini start
+}
+
+do_stop()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini stop
+}
+
+do_restart()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini restart
+}
+
+do_status()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP --daemon etc/production.ini status
+}
+
+do_run()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini
+}
+
+case "$1" in
+  run)
+    do_run
+    ;;
+  start)
+    echo "Starting $PROJECT"
+    do_start
+    ;;
+  stop)
+    echo "Stopping $PROJECT"
+    do_stop
+    ;;
+  status)
+    do_status
+    ;;
+  restart)
+    echo "Restarting $PROJECT"
+    do_restart
+    ;;
+  *)
+    echo "Usage: $0 {run|start|stop|status|restart}" >&2
+    exit 3
+    ;;
+esac
+
+exit 0

bin/manage.sh
@@ -1,0 +1,8 @@
+#!/bin/sh
+
+# Point this to the root of the hshapi install (containing all the bin/, src/, etc/ folders)
+DIR=/Users/tek/src/exosapient
+CMD=$1
+DATE=`date +"%Y-%m-%d.%H-%M"`
+
+cd $DIR && . bin/activate && paster manage -v -c etc/manage.ini $CMD > var/log/manage/$CMD.$DATE.log 2>&1 && deactivate

src/exosapient/commands/admin.py
@@ -1,0 +1,104 @@
+import os
+import paste.fixture
+import paste.registry
+from paste.deploy import loadapp, appconfig
+import paste.deploy.config
+from paste.script.command import Command, BadCommand
+import sys
+
+
+class AdminCommand(Command):
+    """Run the exosapient admin shell"""
+    summary = __doc__.splitlines()[0]
+    group_name = 'exosapient'
+
+    parser = Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', action='store', dest='config_file', type='string',
+                      default='./etc/local.ini', help='Paste deploy config file')
+    parser.add_option('-f', '--file', action='store', dest='script_file', type='string',
+                      help='Python file to run')
+    parser.add_option('--no-ipython', action='store_true', dest='disable_ipython',
+                      help='Do not use the IPython shell')
+    def command(self):
+        self._load_app()
+        try:
+            self.run_shell(*self.args)
+        finally:
+            self._close_app()
+
+    def run_shell(self, *args):
+        locs = dict(__name__="exosapient-admin", wsgiapp=self.wsgiapp, app=self.app)
+
+        admin_boot = os.path.join(os.path.dirname(__file__), 'admin_boot.py')
+        execfile(admin_boot, {}, locs)
+
+        if self.options.script_file:
+            execfile(self.options.script_file, {}, locs)
+            return
+
+        try:
+            # try to use IPython if possible
+            if self.options.disable_ipython:
+                raise ImportError()
+            from IPython.frontend.terminal.embed import InteractiveShellEmbed
+            if self.options.verbose:
+                shell = InteractiveShellEmbed()
+                print ''
+            else:
+                shell = InteractiveShellEmbed(banner1='')
+            shell(local_ns=locs, global_ns={})
+
+        except ImportError:
+            import code
+            shell = code.InteractiveConsole(locals=locs)
+            if self.options.verbose:
+                banner = 'Python %s\n\n' % sys.version
+            else:
+                banner = ''
+
+            try:
+                import readline
+            except ImportError:
+                pass
+
+            print ''
+            shell.interact(banner)
+
+    def _load_app(self):
+        config_file = self.options.config_file
+        if not os.path.isfile(config_file):
+            raise BadCommand('Error: CONFIG_FILE not found at: %s\nPlease specify a CONFIG_FILE' % config_file)
+
+        config_name = 'config:%s' % config_file
+        here_dir = os.getcwd()
+
+        if self.options.verbose:
+            # Configure logging from the config file
+            self.logging_file_config(config_file)
+
+        self.config = appconfig(config_name, relative_to=here_dir)
+        self.config.update({'app_conf': self.config.local_conf,
+                            'global_conf': self.config.global_conf})
+        paste.deploy.config.CONFIG.push_thread_config(self.config)
+
+        # Load locals and populate with objects for use in shell
+        sys.path.insert(0, here_dir)
+
+        # Load the wsgi app first so that everything is initialized right
+        self.wsgiapp = loadapp(config_name, relative_to=here_dir)
+        self.app = paste.fixture.TestApp(self.wsgiapp)
+
+        # Query the test app to setup the environment
+        tresponse = self.app.get('/_test_vars')
+        request_id = int(tresponse.body)
+
+        # Disable restoration during app requests
+        self.app.pre_request_hook = lambda self: paste.registry.restorer.restoration_end()
+        self.app.post_request_hook = lambda self: paste.registry.restorer.restoration_begin(request_id)
+
+        paste.registry.restorer.restoration_begin(request_id)
+
+    def _close_app(self):
+        paste.registry.restorer.restoration_end()
+
+

src/exosapient/commands/manage/__init__.py
@@ -1,0 +1,100 @@
+import os
+import paste.fixture
+import paste.registry
+import paste.deploy.config
+from paste.deploy import loadapp, appconfig
+from paste.script.command import Command, BadCommand
+import sys
+from web.core import config
+
+from exosapient.commands.manage import example
+
+
+COMMANDS = ['show_config', 'show_commands', # system commands
+            'example', # application commands
+            ]
+
+class ManageCommand(Command):
+    """Run exosapient management commands"""
+    summary = __doc__.splitlines()[0]
+    group_name = 'exosapient'
+
+    parser = Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', action='store', dest='config_file', type='string',
+                      default='./etc/manage.ini', help='Paste deploy config file')
+    def command(self):
+        command = self.args.pop(0) if self.args else 'show_commands'
+        if command in COMMANDS and hasattr(self, command)\
+                               and hasattr(getattr(self, command), '__call__'):
+            self._load_app()
+            try:
+                getattr(self, command)(*self.args)
+            finally:
+                self._close_app()
+
+    def show_config(self, *args):
+        """Print the WebCore configuration"""
+        for k in config:
+            print '{k}: {v}'.format(k=k, v=config[k])
+
+    def show_commands(self, *args):
+        """Print a list of available management commands"""
+        print self.__doc__
+        print ''
+        print 'Available commands:'
+        for c in COMMANDS:
+            print '    %s - %s' % (c, getattr(self, c).__doc__)
+        print ''
+
+    def example(self, *args):
+        """This is an example command. Options:
+                     example-option        This options makes the example more examplier
+                     examples=id,id,...    Makes only certain examples examplier
+        """
+        example_option = False
+        examples = None
+        for arg in args:
+            if arg.startswith('example-option') or arg.startswith('example_option'):
+                example_option = True
+            elif arg.startswith('examples='):
+                examples = arg[len('examples='):].split(',')
+        example.perform_example(example_option, examples)
+
+
+    def _load_app(self):
+        config_file = self.options.config_file
+        if not os.path.isfile(config_file):
+            raise BadCommand('Error: CONFIG_FILE not found at: %s\nPlease specify a CONFIG_FILE' % config_file)
+
+        config_name = 'config:%s' % config_file
+        here_dir = os.getcwd()
+
+        if self.options.verbose:
+            # Configure logging from the config file
+            self.logging_file_config(config_file)
+
+        self.config = appconfig(config_name, relative_to=here_dir)
+        self.config.update({'app_conf': self.config.local_conf,
+                            'global_conf': self.config.global_conf})
+        paste.deploy.config.CONFIG.push_thread_config(self.config)
+
+        # Load locals and populate with objects for use in shell
+        sys.path.insert(0, here_dir)
+
+        # Load the wsgi app first so that everything is initialized right
+        wsgiapp = loadapp(config_name, relative_to=here_dir)
+        self.app = paste.fixture.TestApp(wsgiapp)
+
+        # Query the test app to setup the environment
+        tresponse = self.app.get('/_test_vars')
+        request_id = int(tresponse.body)
+
+        # Disable restoration during app requests
+        self.app.pre_request_hook = lambda self: paste.registry.restorer.restoration_end()
+        self.app.post_request_hook = lambda self: paste.registry.restorer.restoration_begin(request_id)
+
+        paste.registry.restorer.restoration_begin(request_id)
+
+    def _close_app(self):
+        paste.registry.restorer.restoration_end()
+

src/exosapient/controllers/root.py
@@ -1,0 +1,12 @@
+from web.core import Controller
+
+from exosapient.util.response import *
+
+log = __import__('logging').getLogger(__name__)
+
+
+class RootController(Controller):
+    def __default__(self, *args, **kwargs):
+        log.info('-- Request --')
+        return TEMPLATE('index', {'project_name': 'exosapient'})
+

src/setup.py
@@ -1,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+        name = "exosapient",
+        version = "0.0.1",
+        description = "__project_description__",
+        author = "__author_name__",
+        author_email = "__author_email__",
+        url = "__project_url__",
+
+        install_requires = [
+            'WebCore<2.0',
+            'flup',
+            'jinja2',
+            'IPython>=0.12',
+            'psutil',
+            ],
+        packages = find_packages(exclude=[
+            'schema'
+            ]),
+
+        zip_safe = False,
+        include_package_data = True,
+        package_data = {
+                '': ['README.md', 'LICENSE'],
+                'exosapient': ['templates/*']
+            },
+
+        paster_plugins = ['PasteScript', 'WebCore'],
+        entry_points = {
+            'paste.paster_command': [
+                'manage = exosapient.commands.manage:ManageCommand',
+                'admin = exosapient.commands.admin:AdminCommand'
+                ]
+            }
+    )

aGHz/aptgregator

bin/initd.sh
@@ -1,0 +1,61 @@
+#! /bin/sh
+
+DIR=/Users/tek/src/aptgregator
+USER=__user__
+GROUP=__group__
+
+PIDFILE=$DIR/var/run/production.pid
+LOGFILE=$DIR/var/log/console.log
+PROJECT=`basename $DIR`
+
+do_start()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini start
+}
+
+do_stop()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini stop
+}
+
+do_restart()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini restart
+}
+
+do_status()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP --daemon etc/production.ini status
+}
+
+do_run()
+{
+    cd $DIR && . bin/activate && paster serve --pid-file=$PIDFILE --log-file=$LOGFILE --user=$USER --group=$GROUP etc/production.ini
+}
+
+case "$1" in
+  run)
+    do_run
+    ;;
+  start)
+    echo "Starting $PROJECT"
+    do_start
+    ;;
+  stop)
+    echo "Stopping $PROJECT"
+    do_stop
+    ;;
+  status)
+    do_status
+    ;;
+  restart)
+    echo "Restarting $PROJECT"
+    do_restart
+    ;;
+  *)
+    echo "Usage: $0 {run|start|stop|status|restart}" >&2
+    exit 3
+    ;;
+esac
+
+exit 0

bin/manage.sh
@@ -1,0 +1,8 @@
+#!/bin/sh
+
+# Point this to the root of the hshapi install (containing all the bin/, src/, etc/ folders)
+DIR=/Users/tek/src/aptgregator
+CMD=$1
+DATE=`date +"%Y-%m-%d.%H-%M"`
+
+cd $DIR && . bin/activate && paster manage -v -c etc/manage.ini $CMD > var/log/manage/$CMD.$DATE.log 2>&1 && deactivate

src/aptgregator/commands/admin.py
@@ -1,0 +1,104 @@
+import os
+import paste.fixture
+import paste.registry
+from paste.deploy import loadapp, appconfig
+import paste.deploy.config
+from paste.script.command import Command, BadCommand
+import sys
+
+
+class AdminCommand(Command):
+    """Run the aptgregator admin shell"""
+    summary = __doc__.splitlines()[0]
+    group_name = 'aptgregator'
+
+    parser = Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', action='store', dest='config_file', type='string',
+                      default='./etc/local.ini', help='Paste deploy config file')
+    parser.add_option('-f', '--file', action='store', dest='script_file', type='string',
+                      help='Python file to run')
+    parser.add_option('--no-ipython', action='store_true', dest='disable_ipython',
+                      help='Do not use the IPython shell')
+    def command(self):
+        self._load_app()
+        try:
+            self.run_shell(*self.args)
+        finally:
+            self._close_app()
+
+    def run_shell(self, *args):
+        locs = dict(__name__="aptgregator-admin", wsgiapp=self.wsgiapp, app=self.app)
+
+        admin_boot = os.path.join(os.path.dirname(__file__), 'admin_boot.py')
+        execfile(admin_boot, {}, locs)
+
+        if self.options.script_file:
+            execfile(self.options.script_file, {}, locs)
+            return
+
+        try:
+            # try to use IPython if possible
+            if self.options.disable_ipython:
+                raise ImportError()
+            from IPython.frontend.terminal.embed import InteractiveShellEmbed
+            if self.options.verbose:
+                shell = InteractiveShellEmbed()
+                print ''
+            else:
+                shell = InteractiveShellEmbed(banner1='')
+            shell(local_ns=locs, global_ns={})
+
+        except ImportError:
+            import code
+            shell = code.InteractiveConsole(locals=locs)
+            if self.options.verbose:
+                banner = 'Python %s\n\n' % sys.version
+            else:
+                banner = ''
+
+            try:
+                import readline
+            except ImportError:
+                pass
+
+            print ''
+            shell.interact(banner)
+
+    def _load_app(self):
+        config_file = self.options.config_file
+        if not os.path.isfile(config_file):
+            raise BadCommand('Error: CONFIG_FILE not found at: %s\nPlease specify a CONFIG_FILE' % config_file)
+
+        config_name = 'config:%s' % config_file
+        here_dir = os.getcwd()
+
+        if self.options.verbose:
+            # Configure logging from the config file
+            self.logging_file_config(config_file)
+
+        self.config = appconfig(config_name, relative_to=here_dir)
+        self.config.update({'app_conf': self.config.local_conf,
+                            'global_conf': self.config.global_conf})
+        paste.deploy.config.CONFIG.push_thread_config(self.config)
+
+        # Load locals and populate with objects for use in shell
+        sys.path.insert(0, here_dir)
+
+        # Load the wsgi app first so that everything is initialized right
+        self.wsgiapp = loadapp(config_name, relative_to=here_dir)
+        self.app = paste.fixture.TestApp(self.wsgiapp)
+
+        # Query the test app to setup the environment
+        tresponse = self.app.get('/_test_vars')
+        request_id = int(tresponse.body)
+
+        # Disable restoration during app requests
+        self.app.pre_request_hook = lambda self: paste.registry.restorer.restoration_end()
+        self.app.post_request_hook = lambda self: paste.registry.restorer.restoration_begin(request_id)
+
+        paste.registry.restorer.restoration_begin(request_id)
+
+    def _close_app(self):
+        paste.registry.restorer.restoration_end()
+
+

src/aptgregator/commands/manage/__init__.py
@@ -1,0 +1,100 @@
+import os
+import paste.fixture
+import paste.registry
+import paste.deploy.config
+from paste.deploy import loadapp, appconfig
+from paste.script.command import Command, BadCommand
+import sys
+from web.core import config
+
+from aptgregator.commands.manage import example
+
+
+COMMANDS = ['show_config', 'show_commands', # system commands
+            'example', # application commands
+            ]
+
+class ManageCommand(Command):
+    """Run aptgregator management commands"""
+    summary = __doc__.splitlines()[0]
+    group_name = 'aptgregator'
+
+    parser = Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', action='store', dest='config_file', type='string',
+                      default='./etc/manage.ini', help='Paste deploy config file')
+    def command(self):
+        command = self.args.pop(0) if self.args else 'show_commands'
+        if command in COMMANDS and hasattr(self, command)\
+                               and hasattr(getattr(self, command), '__call__'):
+            self._load_app()
+            try:
+                getattr(self, command)(*self.args)
+            finally:
+                self._close_app()
+
+    def show_config(self, *args):
+        """Print the WebCore configuration"""
+        for k in config:
+            print '{k}: {v}'.format(k=k, v=config[k])
+
+    def show_commands(self, *args):
+        """Print a list of available management commands"""
+        print self.__doc__
+        print ''
+        print 'Available commands:'
+        for c in COMMANDS:
+            print '    %s - %s' % (c, getattr(self, c).__doc__)
+        print ''
+
+    def example(self, *args):
+        """This is an example command. Options:
+                     example-option        This options makes the example more examplier
+                     examples=id,id,...    Makes only certain examples examplier
+        """
+        example_option = False
+        examples = None
+        for arg in args:
+            if arg.startswith('example-option') or arg.startswith('example_option'):
+                example_option = True
+            elif arg.startswith('examples='):
+                examples = arg[len('examples='):].split(',')
+        example.perform_example(example_option, examples)
+
+
+    def _load_app(self):
+        config_file = self.options.config_file
+        if not os.path.isfile(config_file):
+            raise BadCommand('Error: CONFIG_FILE not found at: %s\nPlease specify a CONFIG_FILE' % config_file)
+
+        config_name = 'config:%s' % config_file
+        here_dir = os.getcwd()
+
+        if self.options.verbose:
+            # Configure logging from the config file
+            self.logging_file_config(config_file)
+
+        self.config = appconfig(config_name, relative_to=here_dir)
+        self.config.update({'app_conf': self.config.local_conf,
+                            'global_conf': self.config.global_conf})
+        paste.deploy.config.CONFIG.push_thread_config(self.config)
+
+        # Load locals and populate with objects for use in shell
+        sys.path.insert(0, here_dir)
+
+        # Load the wsgi app first so that everything is initialized right
+        wsgiapp = loadapp(config_name, relative_to=here_dir)
+        self.app = paste.fixture.TestApp(wsgiapp)
+
+        # Query the test app to setup the environment
+        tresponse = self.app.get('/_test_vars')
+        request_id = int(tresponse.body)
+
+        # Disable restoration during app requests
+        self.app.pre_request_hook = lambda self: paste.registry.restorer.restoration_end()
+        self.app.post_request_hook = lambda self: paste.registry.restorer.restoration_begin(request_id)
+
+        paste.registry.restorer.restoration_begin(request_id)
+
+    def _close_app(self):
+        paste.registry.restorer.restoration_end()
+

src/aptgregator/controllers/root.py
@@ -1,0 +1,12 @@
+from web.core import Controller
+
+from aptgregator.util.response import *
+
+log = __import__('logging').getLogger(__name__)
+
+
+class RootController(Controller):
+    def __default__(self, *args, **kwargs):
+        log.info('-- Request --')
+        return TEMPLATE('index', {'project_name': 'aptgregator'})
+

src/setup.py
@@ -1,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+        name = "aptgregator",
+        version = "0.0.1",
+        description = "__project_description__",
+        author = "__author_name__",
+        author_email = "__author_email__",
+        url = "__project_url__",
+
+        install_requires = [
+            'WebCore<2.0',
+            'flup',
+            'jinja2',
+            'IPython>=0.12',
+            'psutil',
+            ],
+        packages = find_packages(exclude=[
+            'schema'
+            ]),
+
+        zip_safe = False,
+        include_package_data = True,
+        package_data = {
+                '': ['README.md', 'LICENSE'],
+                'aptgregator': ['templates/*']
+            },
+
+        paster_plugins = ['PasteScript', 'WebCore'],
+        entry_points = {
+            'paste.paster_command': [
+                'manage = aptgregator.commands.manage:ManageCommand',
+                'admin = aptgregator.commands.admin:AdminCommand'
+                ]
+            }
+    )